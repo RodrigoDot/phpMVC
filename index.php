fc9f9f0b3ec9f09e7d6c0ec87bfaa8216494f1fc
RodrigoDot <rodrigodot@outlook.com>
2017-05-25 19:24:46 -0300

    spacenames OK

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>PHP COM MVC</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css" />
+</head>
+<body>
+</body>
+</html>
