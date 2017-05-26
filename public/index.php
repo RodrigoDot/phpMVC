d6279c6119b4ba1986c0ead329dd27c2d9c67883
RodrigoDot <rodrigodot@outlook.com>
2017-05-25 21:24:36 -0300

    inicializacao do composer OK

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
+<?php
+    require_once("../vendor/autoload.php");
+?>
 <html lang="pt-br">
 <head>
     <title>PHP COM MVC</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../lib/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../lib/bootstrap/bootstrap.min.css" />
 </head>
 <body>
+INIT
 </body>
 </html>
